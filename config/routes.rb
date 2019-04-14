5e5c4276fc576cc1724e660e1a9402a4400fdcc2
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-04-13 19:13:07 -0700

    routes resource update

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 Rails.application.routes.draw do
-  get '/tasks', to: 'tasks#index', as: 'tasks'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # method path-URL, to: Controller#action (order of routes matter, need #new before #show)
+  root to: 'tasks#index'
 
-  get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  post '/tasks', to: 'tasks#create'
-  
-  get '/tasks/:id', to: 'tasks#show', as: 'task' #id does not have to be a number, it can be a word ORDER MATTERS OF ROUTES
-  
-  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  patch '/tasks/:id', to: 'tasks#update'
+  resources :tasks
 
   get '/tasks/:id/toggle_complete', to: 'tasks#toggle_complete', as: 'toggle_complete'
   patch '/tasks/:id/toggle_complete', to: 'tasks#toggle_complete'
 
   get '/tasks/:id/verify', to: 'tasks#verify', as: 'verify'
-  delete '/tasks/:id/', to: 'tasks#destroy'
-
-  root to: 'tasks#index' # makes homepage to site go to the index, also get '/', to: 'tasks#index'
 end
